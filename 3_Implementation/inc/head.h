f0f87b8d62818c54a9f729e67a0ed88ffe24eb99
Parikshit Shukla <94338490+sparikshit@users.noreply.github.com>
2021-11-25 02:23:31 +0530

    Add files via upload

---
+++ 3_Implementation/inc/head.h
@@ -0,0 +1,29 @@
+/**
+ * @file Quiz Game
+ * @author Parikshit
+ * @brief It's a Quiz Game
+ * @version 0.1
+ * @date 2021-11-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ * 
+ */
+
+#ifndef _head_H
+#define _head_H
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+void calculateScore();
+/* To Calculate Score */
+void question();
+/* To Print Questions */
+void menu();
+/* To Print Menu */
+void main();
+/* To Record the Name*/
+
+#endif
